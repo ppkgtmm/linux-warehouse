74ad82c5e8c2a7b84832fd19805553f73f4fdfce
Pinky Gautam <pkgtm1998@gmail.com>
2024-03-16 21:53:55 +0700

    Create fbjobs.sh

---
+++ code/fbjobs.sh
@@ -0,0 +1,3 @@
+top -o mem & # executes top command in background
+bg %2 # number for job or stopped process is known from jobs command
+fg %2 # number for job or stopped process is known from jobs command
